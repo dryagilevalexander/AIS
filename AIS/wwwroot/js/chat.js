e174b4e8efad3367a6a084b3791572a426eb8cd4
DryagilevAlexander <dryagilevalexander@gmail.com>
2023-06-30 12:10:33 +0300

    Чат. Правка отображения.

---
+++ AIS/wwwroot/js/chat.js
@@ -0,0 +1,9 @@
+﻿function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("openChat").setAttribute("style", "display: none;");
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("openChat").setAttribute("style", "display: block;");
+}
